42b9c0ef3f5eab6bb43c32d5b3b8730b2d116e08
Kest <arinchaik1994@gmail.com>
2022-10-17 22:34:48 +0700

    make a beautiful footer slider (marquee)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const Footer = () => {
-	return <div className="footer">&#169; Made by CodeWizard_26</div>;
+	return <div className="footer">
+		<Marquee style={{ width: '100%', height: '100%' }}>
+			&#169; Made by CodeWizard_26
+		</Marquee>
+	</div>;
 };
 
 export default Footer;
